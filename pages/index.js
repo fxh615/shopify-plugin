d26a8a16d7cf2bbf1a32cc05a71ce974c8673e6f
haven feng <haven@imile.me>
2019-12-18 20:55:51 +0800

    update

--- pages/index.js
+++ pages/index.js
@@ -8,26 +8,27 @@
  */
 
 import Cookies from "js-cookie";
-var shopifyAPI = require('shopify-node-api');
+import shopifyAPI from 'shopify-node-api';
 
 class Index extends React.Component {
-  
-
-  componentDidMount(){
-
+  static getInitialProps({ res, err }) {
     var Shopify = new shopifyAPI({
-      shop: 'imile-dev.myshopify.com', // MYSHOP.myshopify.com
+      shop: 'imile-dev',
       shopify_api_key: 'dc0bcabedc2602c2fa2cee929e4dee0d', // Your API key
       shopify_shared_secret: '660330275b6db74d79eff3e06d3b1cd2', // Your Shared Secret
       shopify_scope: 'read_shipping'
     });
 
-
-
     Shopify.get('/admin/api/2019-10/carrier_services.json', function(err, data, headers){
       console.log(data); // Data contains product json information
       console.log(headers); // Headers returned from request
+      console.log('Headers returned from request'); // Headers returned from request
     });
+  }
+
+  componentDidMount(){
+
+
   }
   render() {
     
