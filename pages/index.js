2ad1baf80a4c47a0724c7f3162692d76d18cf1b9
haven feng <haven@imile.me>
2019-12-18 21:12:45 +0800

    update

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ class Index extends React.Component {
       shop: 'imile-dev',
       shopify_api_key: 'dc0bcabedc2602c2fa2cee929e4dee0d', // Your API key
       shopify_shared_secret: '660330275b6db74d79eff3e06d3b1cd2', // Your Shared Secret
-      shopify_scope: 'read_shipping'
+      shopify_scope: 'write_shipping'
     });
 
     Shopify.get('/admin/api/2019-10/carrier_services.json', function(err, data, headers){
