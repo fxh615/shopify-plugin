3a308d103eb04382ea739bc510e392e8a8a02a88
haven feng <haven@imile.me>
2019-12-19 11:05:59 +0800

    update

--- pages/index.js
+++ pages/index.js
@@ -12,20 +12,14 @@ import shopifyAPI from 'shopify-node-api';
 
 class Index extends React.Component {
   static getInitialProps({ res, err }) {
-
-
-    return {}
-  }
-
-  componentDidMount(){
-    console.log("Cookies.get('accessToken')", Cookies.get('accessToken'));
+    console.log("res.accessToken", res.accessToken);
     var Shopify = new shopifyAPI({
       shop: 'imile-dev',
       shopify_api_key: 'dc0bcabedc2602c2fa2cee929e4dee0d', // Your API key
       // shopify_shared_secret: '660330275b6db74d79eff3e06d3b1cd2', // Your Shared Secret
       // shopify_scope: 'read_shipping',
       // redirect_uri: 'https://www.10dang.com/auto/callback',
-      access_token: Cookies.get('accessToken')
+      access_token: res.accessToken
     });
 
     Shopify.get('/admin/api/2019-10/carrier_services.json', function(err, data, headers){
@@ -33,6 +27,12 @@ class Index extends React.Component {
       console.log('headers', headers); // Headers returned from request
       console.log('Headers returned from request'); // Headers returned from request
     });
+
+    return {}
+  }
+
+  componentDidMount(){
+
 
   }
   render() {
