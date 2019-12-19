18887e7c1a6854badf019075aa3fcf93193279ca
haven feng <haven@imile.me>
2019-12-19 20:26:38 +0800

    update

--- server.js
+++ server.js
@@ -114,17 +114,37 @@ app.prepare().then(() => {
   });
 
   router.post('/getRate', ctx=>{
-    let arr = [
-      {
-        "service_name": "imile sv0001",
-        "description": "description",
-        "service_code": "43543254325432543",
-        "currency": "USD",
-        "total_price": '1344'
-      }
-    ];
+    let rates = {
+      "rates": [
+          {
+              "service_name": "canadapost-overnight",
+              "service_code": "ON",
+              "total_price": "1295",
+              "description": "This is the fastest option by far",
+              "currency": "CAD",
+              "min_delivery_date": "2013-04-12 14:48:45 -0400",
+              "max_delivery_date": "2013-04-12 14:48:45 -0400"
+          },
+          {
+              "service_name": "fedex-2dayground",
+              "service_code": "2D",
+              "total_price": "2934",
+              "currency": "USD",
+              "min_delivery_date": "2013-04-12 14:48:45 -0400",
+              "max_delivery_date": "2013-04-12 14:48:45 -0400"
+          },
+          {
+              "service_name": "fedex-priorityovernight",
+              "service_code": "1D",
+              "total_price": "3587",
+              "currency": "USD",
+              "min_delivery_date": "2013-04-12 14:48:45 -0400",
+              "max_delivery_date": "2013-04-12 14:48:45 -0400"
+          }
+      ]
+   }
 
-    ctx.body = arr;
+    ctx.body = rates;
     ctx.res.statusCode = 200;
   })
 
